495c8ef0302f12b2aa3fe359784793b94bbb2f9c
Piyushdigraskar <piyushdigraskar3@gmail.com>
2023-05-16 14:33:22 +0530

    Tag and class

---
+++ dom2.js
@@ -0,0 +1,4 @@
+var li =document.getElementsByTagName('li')
+var items = document.getElementsByClassName('list-group-item');
+items[4].style.backgroundColor ='yellow';
+//li[4].style.backgroundColor ='green';
