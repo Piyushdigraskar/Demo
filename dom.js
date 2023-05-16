bc80a25fc89dd8cd827384724bd5e620e2bedf5d
Piyushdigraskar <piyushdigraskar3@gmail.com>
2023-05-16 12:04:54 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header =document.getElementById('header-title');
+header.style.borderBottom = 'solid 3px #000';
+var title =document.getElementByClass('title')
+title.style.fontweight ='bold';
+title.style.color ='green';
